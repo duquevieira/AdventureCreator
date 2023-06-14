01866754a6b68e9cf0d54f561b06023e524126fd
andreauxtero <a.auxtero@campus.fct.unl.pt>
2023-06-14 01:18:19 +0100

    Added testing mode
    Switched the "switch button" to a dropdown having 3 options: map,storyboard and testing modes.
    All 3 modes are working.

--- Assets/Scripts/SwitchCreateMode.cs
+++ Assets/Scripts/SwitchCreateMode.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchCreateMode : MonoBehaviour
 {
     [HideInInspector] public enum CreateMode {MapMode, StoryBoardMode, TestingMode};
     public CreateMode currentMode;
+    [SerializeField] private TMP_Dropdown dropdown;
 
     // Start is called before the first frame update
     void Start()
     {
         currentMode = CreateMode.MapMode;
+        dropdown.onValueChanged.AddListener(delegate { DropDownValueChanged(dropdown); });
     }
 
-    public void SwitchMode()
+    /*public void SwitchMode()
     {
         if (currentMode == CreateMode.MapMode)
         {
@@ -22,5 +26,25 @@ public class SwitchCreateMode : MonoBehaviour
         {
             currentMode = CreateMode.MapMode;
         }
+    }*/
+
+    private void DropDownValueChanged(TMP_Dropdown change)
+    {
+        switch (change.value)
+        {
+            case 0:
+                currentMode = CreateMode.MapMode;
+                break;
+            case 1:
+                currentMode = CreateMode.StoryBoardMode;
+                break;
+            case 2:
+                currentMode = CreateMode.TestingMode;
+                break;
+            default:
+                currentMode = CreateMode.MapMode;
+                break;
+        }
+        
     }
 }
