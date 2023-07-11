84af1c12bb3d2843a98e0580c1618362f168bfff
Daniel-Gavinho <d.gavinho@campus.fct.unl.pt>
2023-07-11 23:12:08 +0100

    Save and Load Finished

--- Assets/Scripts/Records/Game.cs
+++ Assets/Scripts/Records/Game.cs
@@ -7,12 +7,14 @@ public class Game
     public string LevelId;
     public PositionCoordinates Player;
     public List<StoryboardStep> Storyboard;
-    public List<ItemGroup> Inventory;
+    public List<ItemGroup> StoryItems;
+    public List<ItemGroup> InventoryItems;
 
     public Game(string level, StoryEngineScript story) {
         LevelId = level;
         Player = new PositionCoordinates(story.Player.transform.position.x, story.Player.transform.position.z);
         Storyboard = story.Storyboard;
-        Inventory = story.StoryItems;
+        StoryItems = story.StoryItems;
+        InventoryItems = story.InventoryItems;
     }
 }
