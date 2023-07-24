cd66cdbfcb4be80ac24c6eb429405db075dec0e3
Daniel-Gavinho <d.gavinho@campus.fct.unl.pt>
2023-07-24 16:24:40 +0100

    Fixing animation
    -No more twitchiness

--- Assets/Scripts/UI/SimplePressAnimation.cs
+++ Assets/Scripts/UI/SimplePressAnimation.cs
@@ -1,19 +1,24 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
 public class SimplePressAnimation : MonoBehaviour
 {
     private const float ROTATE_SPEED = 360f;
-    public void PlayAnimation() {
+
+    public void PlayAnimation()
+    {
         StartCoroutine(SpinOnce());
     }
 
-    private IEnumerator SpinOnce() {
+    private IEnumerator SpinOnce()
+    {
         float targetAngle = transform.eulerAngles.y + 360f;
         float elapsedTime = 0f;
+        float startAngle = transform.eulerAngles.y;
 
-        while(elapsedTime < 1f) {
-            float currentAngle = Mathf.Lerp(transform.eulerAngles.y, targetAngle, elapsedTime);
+        while (elapsedTime < 1f)
+        {
+            float currentAngle = Mathf.Lerp(startAngle, targetAngle, elapsedTime);
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, currentAngle, transform.eulerAngles.z);
             elapsedTime += Time.deltaTime;
             yield return null;
