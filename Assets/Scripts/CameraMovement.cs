70ca81ecf2eb547b25bcf0c9aee9a6de42e4e747
andreauxtero <a.auxtero@campus.fct.unl.pt>
2023-09-19 23:16:07 +0100

    Keyboard camera movement removal
    Removal of keyboard camera movement because it conflited with writing the save name.
    In later iterations, should stop moving camera if writing on save name.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -26,7 +26,7 @@ public class CameraMovement : MonoBehaviour
 
     private void HandleCameraMovement()
     {
-        KeyBoardMovement();
+        //KeyBoardMovement();
         DragPanMovement();
     }
 
